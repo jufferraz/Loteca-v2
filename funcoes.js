7bbe5b1499f185cbd9775b8918a3d8ee8490fd53
jufferraz <martinsjulia091@gmail.com>
2022-08-24 10:12:36 -0400

    criamos funções js

---
+++ funcoes.js
@@ -0,0 +1,27 @@
+var numEsco = []
+var numSort = []
+
+function sortNumeros(){
+    for(let i = 0; i < 6; i ++){
+    let num = Math.ceil(Math.random() * 60)
+    console.log(num)
+    if(!numSort.includes(num)){
+        numSort[i] = num
+    } else {
+        i = i -1;
+    }
+  } 
+  console.log(numSort)
+}
+function lerNumero(value, pos){
+    if(value < 1){
+        alert("Número não pode ser 0 ou negativo! Digite novamente.")
+    } else if(value > 60){
+        alert("Número não pode ser maior que 60! Digite novamente.")
+    } else if(numEsco.includes(value)){
+        alert("Número repetido! Digite novamente.")
+    } else{
+    numEsco[pos] = value
+    console.log(numEsco)
+    }
+}
